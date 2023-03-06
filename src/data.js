97c3e7ff4257207a9f9f063cb379da5d627e1e02
LadyDi3103 <chinamy2004@gmail.com>
2023-03-06 10:19:26 -0500

    Se implementó interfaz responsive y se trabajó en el menú desplegable

--- src/data.js
+++ src/data.js
@@ -1,9 +1,22 @@
 // estas funciones son de ejemplo
 
 export const example = () => {
-  return 'example';
+  return "example";
 };
 
 export const anotherExample = () => {
-  return 'OMG';
+  return "OMG";
 };
+// El corazón de este proyecto es la manipulación de datos a través de arreglos y objetos.
+
+// Te reomendamos que este archivo contenga toda la funcionalidad que corresponda a obtener, procesar y manipular datos (tus funciones). Por ejemplo:
+
+// filterData(data, condition): esta función filter o filtrar recibiría la data, y nos retornaría aquellos datos que sí cumplan con la condición.
+
+// sortData(data, sortBy, sortOrder): esta función sort u ordenar recibe tres parámetros. El primer parámetro, data, nos entrega los datos. El segundo parámetro, sortBy, nos dice con respecto a cuál de los campos de la data se quiere ordenar. El tercer parámetro, sortOrder, indica si se quiere ordenar de manera ascendente o descendente.
+
+// computeStats(data): la función compute o calcular, nos permitirá hacer cálculos estadísticos básicos para ser mostrados de acuerdo a la data proporcionada.
+
+// Estos nombres de funciones y de parámetros son solamente referenciales, lo que decidas depende de tu propia implementación.
+
+// Estas funciones deben ser puras e independientes del DOM. Estas funciones serán después usadas desde el archivo src/main.js, al cargar la página, y cada vez que el usuario interactúe (click, filtrado, ordenado, ...).
--- src/main.js
+++ src/main.js
@@ -1,6 +1,10 @@
-import { example } from './data.js';
+import { example } from "./data.js";
 // import data from './data/lol/lol.js';
-import data from './data/pokemon/pokemon.js';
+import data from "./data/pokemon/pokemon.js";
 // import data from './data/rickandmorty/rickandmorty.js';
 
 console.log(example, data);
+
+// src/main.js para todo tu código que tenga que ver con mostrar los datos en la pantalla. Con esto nos referimos básicamente a la interacción con el DOM. Operaciones como creación de nodos, registro de manejadores de eventos (event listeners o event handlers),
+
+// Y ahora tendríamos la variable data disponible en el script src/main.js.
