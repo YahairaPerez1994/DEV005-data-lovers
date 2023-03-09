d9cb979d375216e1ab6f7d2dcf0c9f89dad700c5
YahairaPerez1994 <yahairaperez2694@gmail.com>
2023-03-09 00:52:13 -0500

    Repara el error de los nuevos cambios

--- src/main.js
+++ src/main.js
@@ -1,10 +1,17 @@
-import { example } from "./data.js";
-// import data from './data/lol/lol.js';
+//import { example } from "./data.js";
+//import data from './data/lol/lol.js';
 import data from "./data/pokemon/pokemon.js";
-// import data from './data/rickandmorty/rickandmorty.js';
 
-console.log(example, data);
+//const btnEmpezar = document.getElementById("empezar");
+const btnEmpezar = document.getElementById("empezar");
+btnEmpezar.addEventListener("click", muestraSeccionContainer);
 
+const section0 = document.getElementById("section0");
+const container = document.getElementById("container");
+function muestraSeccionContainer() {
+  section0.style.display = "none";
+  container.style.display = "block"; 
+}
 // src/main.js para todo tu código que tenga que ver con mostrar los datos en la pantalla. Con esto nos referimos básicamente a la interacción con el DOM. Operaciones como creación de nodos, registro de manejadores de eventos (event listeners o event handlers),
 
 // Y ahora tendríamos la variable data disponible en el script src/main.js.
