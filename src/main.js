f53665311cb7ac1cf204b5e5b1eb6cd81af09ba9
YahairaPerez1994 <yahairaperez2694@gmail.com>
2023-03-15 14:07:24 -0500

    Ordena de la (A-Z), y muestra todos los pokemones

--- src/main.js
+++ src/main.js
@@ -1,17 +1,132 @@
 //import { example } from "./data.js";
 //import data from './data/lol/lol.js';
-import data from "./data/pokemon/pokemon.js";
+import data from './data/pokemon/pokemon.js';
+import {sortData} from "./data.js";
 
-//const btnEmpezar = document.getElementById("empezar");
-const btnEmpezar = document.getElementById("empezar");
-btnEmpezar.addEventListener("click", muestraSeccionContainer);
+const pokemones = data.pokemon;
+
+// console.log(sortByName(pokemones));
+const headerHtml = document.getElementById("headerHtml");
+headerHtml.addEventListener("onload", ocultarHeader());
 
 const section0 = document.getElementById("section0");
-const container = document.getElementById("container");
-function muestraSeccionContainer() {
+const container = document.getElementById("contieneTodosLosPokemones");
+
+const btnEmpezar = document.getElementById("empezar");
+btnEmpezar.addEventListener("click", mostrarSeccionContainer());
+
+function mostrarSeccionContainer() {
   section0.style.display = "none";
-  container.style.display = "block"; 
+  mostrarHeader();
+  container.style.display = "block";
 }
+
+function ocultarHeader() {
+  headerHtml.style.display = "none";
+}
+
+function mostrarHeader() {
+  headerHtml.style.display = "block";
+}
+//Todos: hace referencia a las opciones del select
+//Chequear: para agregar lo del EVENT
+const sortTodos = document.getElementById("todos");
+sortTodos.addEventListener ("click", pintar(pokemones));
+
+// function muestraTodoslosCards(array) {
+//   //console.log("ver que llega en cada caso: ", array);
+//   const tagDivGeneral = document.createElement("div");
+//   tagDivGeneral.className = "ContieneTodosLosPokemones";
+//   document.getElementById("container").appendChild(tagDivGeneral);
+//   for (let i = 0; i < array.length; i++) {
+
+//     const tagDivHijoGeneral = document.createElement("div");
+//     tagDivHijoGeneral.className = "Cards";
+//     tagDivGeneral.appendChild(tagDivHijoGeneral);
+
+//     const tagPhp = document.createElement("p");
+//     tagPhp.className = "hp";
+//     tagPhp.textContent = array[i].stats.base_stat;
+
+//     tagDivHijoGeneral.appendChild(tagPhp);
+
+//     const tagSpan = document.createElement("span");
+//     tagSpan.textContent = "HP";
+//     tagPhp.appendChild(tagSpan);
+
+//     const imgPokemon = document.createElement("img");
+//     tagDivHijoGeneral.appendChild(imgPokemon);
+//     imgPokemon.src = array[i].img;
+//     imgPokemon.alt = "Imágenes de Pokemones";
+//     imgPokemon.className = "imgCard";
+
+//     const tagPname = document.createElement("p");
+//     tagPname.textContent = array[i].name;
+//     tagPname.className = "poke-name";
+//     tagDivHijoGeneral.appendChild(tagPname);
+
+//     const tagPnum = document.createElement("p");
+//     tagPnum.textContent = array[i].num;
+//     tagPnum.className = "poke-number";
+//     tagDivHijoGeneral.appendChild(tagPnum);
+
+//     const tagPtype = document.createElement("p");
+//     tagPtype.className = "types";
+//     tagPtype.textContent = array[i].type;
+//     tagDivHijoGeneral.appendChild(tagPtype);
+//   }
+// } 
+
+function pintar(data){
+  let todos = '';
+  //data.forEach((elemento) => {
+  for (let i = 0; i < data.length; i++){
+    todos += `
+<div class="Cards">
+<p class="hp"><span>HP </span>${data[i].stats["max-hp"]}</p>
+<img src=${data[i].img} alt="Imágenes de Pokemones" class="imgCard">
+<p class="poke-name">${data[i].name.toUpperCase()}</p>
+<p class="poke-number">Nº ${data[i].num}</p>
+<p class="types">${data[i].type}</p>
+</div>
+   `
+  }
+  document.getElementById('contieneTodosLosPokemones').innerHTML = todos;
+}
+pintar(pokemones);
+//console.log(pokemones);
+//console.log(pintar(pokemones))
+
+// const sortAZ = document.getElementById("A-Z");
+// sortAZ.addEventListener ("click",()=>{
+//   console.log('se dio click');
+//   document.getElementById("container").innerHTML=""
+//   muestraTodoslosCards(sortByName(pokemones))
+// })
+
+document.getElementById("ordenar").addEventListener("change", (event) => {
+  //console.log("veamos el evento", event.target.value);
+  const option = event.target.value;
+  if (option === "TODOS") {
+    pintar(pokemones);
+    //muestraTodoslosCards(pokemones);
+  } else if (option === "A-Z") {
+    const resultadoAZ = sortData(pokemones);
+    pintar(resultadoAZ);
+  } else {
+    const resultadoZA = sortData(pokemones); 
+    pintar(resultadoZA.reverse);
+  }
+});
+
+// function muestralosCardsOrdenadosdelaAZ() {
+// }
+
+// const sortZA = document.getElementById("Z-A");
+// sortAZ.addEventListener ("click", muestralosCardsOrdenadodelaZA());
+// function muestralosCardsOrdenadosdelaZA() {
+// }
+
 // src/main.js para todo tu código que tenga que ver con mostrar los datos en la pantalla. Con esto nos referimos básicamente a la interacción con el DOM. Operaciones como creación de nodos, registro de manejadores de eventos (event listeners o event handlers),
 
 // Y ahora tendríamos la variable data disponible en el script src/main.js.
@@ -27,45 +142,44 @@ function muestraSeccionContainer() {
 // }
 
 // document.getElementById("pokemonesDisplay").appendChild(fragmentName);
+//Function que estructure los cards
 
-const tagDivGeneral = document.createElement("div");
-tagDivGeneral.className = "ContieneTodosLosPokemones";
+// for (let i = 0; i < data.pokemon.length; i++) {
+//   document.getElementById("container").appendChild(tagDivGeneral);
 
-for (let i = 0; i < data.pokemon.length; i++) {
-  document.getElementById("container").appendChild(tagDivGeneral);
+//   const tagDivHijoGeneral = document.createElement("div");
+//   tagDivHijoGeneral.className = "Cards";
+//   tagDivGeneral.appendChild(tagDivHijoGeneral);
 
-  const tagDivHijoGeneral = document.createElement("div");
-  tagDivHijoGeneral.className = "Cards";
-  tagDivGeneral.appendChild(tagDivHijoGeneral);
+//   const tagPhp = document.createElement("p");
+//   tagPhp.className = "hp";
+//   tagPhp.textContent = data.pokemon[i].stats.base_stat;
 
-  const tagPhp = document.createElement("p");
-  tagPhp.className = "hp";
-  tagPhp.textContent = data.pokemon[i].stats.base_stat;
+//   tagDivHijoGeneral.appendChild(tagPhp);
 
-  tagDivHijoGeneral.appendChild(tagPhp);
+//   const tagSpan = document.createElement("span");
+//   tagSpan.textContent = "HP";
+//   tagPhp.appendChild(tagSpan);
 
-  const tagSpan = document.createElement("span");
-  tagSpan.textContent = "HP";
-  tagPhp.appendChild(tagSpan);
+//   const imgPokemon = document.createElement("img");
+//   tagDivHijoGeneral.appendChild(imgPokemon);
+//   imgPokemon.src = data.pokemon[i].img;
+//   imgPokemon.alt = "Imágenes de Pokemones";
+//   imgPokemon.className = "imgCard";
 
-  const imgPokemon = document.createElement("img");
-  tagDivHijoGeneral.appendChild(imgPokemon);
-  imgPokemon.src = data.pokemon[i].img;
-  imgPokemon.alt = "Imágenes de Pokemones";
-  imgPokemon.className = "imgCard";
+//   const tagPname = document.createElement("p");
+//   tagPname.textContent = data.pokemon[i].name;
+//   tagPname.className = "poke-name";
+//   tagDivHijoGeneral.appendChild(tagPname);
 
-  const tagPname = document.createElement("p");
-  tagPname.textContent = data.pokemon[i].name;
-  tagPname.className = "poke-name";
-  tagDivHijoGeneral.appendChild(tagPname);
+//   const tagPnum = document.createElement("p");
+//   tagPnum.textContent = data.pokemon[i].num;
+//   tagPnum.className = "poke-number";
+//   tagDivHijoGeneral.appendChild(tagPnum);
 
-  const tagPnum = document.createElement("p");
-  tagPnum.textContent = data.pokemon[i].num;
-  tagPnum.className = "poke-number";
-  tagDivHijoGeneral.appendChild(tagPnum);
-
-  const tagPtype = document.createElement("p");
-  tagPtype.className = "types";
-  tagPtype.textContent = data.pokemon[i].type;
-  tagDivHijoGeneral.appendChild(tagPtype);
-}
+//   const tagPtype = document.createElement("p");
+//   tagPtype.className = "types";
+//   tagPtype.textContent = data.pokemon[i].type;
+//   tagDivHijoGeneral.appendChild(tagPtype);
+// }
+// //console.log (data.pokemon);
